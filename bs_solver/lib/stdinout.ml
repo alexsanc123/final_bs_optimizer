9566fb25313786d82ec6dbcfd2af1aa7b639b187
Jokorie <jokorie@mit.edu>
2023-07-28 17:54:28 +0000

    loop message prompt

--- bs_solver/lib/stdinout.ml
+++ bs_solver/lib/stdinout.ml
@@ -5,7 +5,7 @@ let rec stdin_reprompt
   ~(prompt : string)
   ~(form_checker : string -> bool)
   ?(looped = false)
-  ?(added_prompt = "Invalid Input Form: ")
+  ?(added_prompt = "")
   ()
   =
   let new_prompt = if looped then added_prompt ^ prompt else prompt in
@@ -18,13 +18,13 @@ let rec stdin_reprompt
     stdin_reprompt ~prompt ~form_checker ~looped:true ~added_prompt ()
 ;;
 
-let alphabet = "tjqka"
-let digits = "1234567890"
+(* let alphabet = "tjqka" *)
+let card_chars = "123456789tjqka"
 
 let card_form_checker input =
   match String.length input with
   | 1 ->
-    if String.contains alphabet (Char.lowercase (String.get input 0))
+    if String.contains card_chars (Char.lowercase (String.get input 0))
     then true
     else false
   | _ -> false
@@ -36,4 +36,12 @@ let num_form_checker input =
   | _ ->
     let num = Int.of_string_opt input in
     (match num with Some _ -> true | None -> false)
+;;
+
+let loop_card_input ~prompt =
+  stdin_reprompt ~prompt ~form_checker:card_form_checker ()
+;;
+
+let loop_num_input ~prompt =
+  stdin_reprompt ~prompt ~form_checker:num_form_checker ()
 ;;
---
+++ bs_solver/lib/stdinout.mli
@@ -0,0 +1,2 @@
+val loop_card_input : prompt:string -> string
+val loop_num_input : prompt:string -> string
