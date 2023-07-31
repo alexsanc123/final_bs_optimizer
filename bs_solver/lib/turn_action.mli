28d3608b8d4df570f5398860958d4592f7a6fca2
Alexandra Sanchez <alexsanc@mit.edu>
2023-07-31 18:19:56 +0000

    added eval strat to mli

--- bs_solver/lib/turn_action.mli
+++ bs_solver/lib/turn_action.mli
@@ -4,3 +4,11 @@ val lie_with_last_card
   :  win_cycle:(Card.t * int) list
   -> strategy:Strategy.t
   -> Strategy.t
+
+val quantify_bluffs : strategy:Strategy.t -> int
+val _evaluate_strategies : strategies:Strategy.t list -> Strategy.t
+
+val _act_on_strategy
+  :  strategy:Strategy.t
+  -> card_to_provide:Card.t
+  -> Card.t list
