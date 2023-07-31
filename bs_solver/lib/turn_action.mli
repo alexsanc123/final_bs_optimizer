1e09a69adc3f0ca4e22361260da0d001081eb729
Alexandra Sanchez <alexsanc@mit.edu>
2023-07-31 18:23:44 +0000

    changed evaluate strategy

--- bs_solver/lib/turn_action.mli
+++ bs_solver/lib/turn_action.mli
@@ -6,7 +6,7 @@ val lie_with_last_card
   -> Strategy.t
 
 val quantify_bluffs : strategy:Strategy.t -> int
-val _evaluate_strategies : strategies:Strategy.t list -> Strategy.t
+val _evaluate_strategies : win_cycle:(Card.t * int) list -> Strategy.t
 
 val _act_on_strategy
   :  strategy:Strategy.t
