067d6a6f8fb97281d54e04a3e862c032cb832af5
Jokorie <jokorie@mit.edu>
2023-08-11 15:58:56 +0000

    recent push

--- frontend/src/components/StartPage.js
+++ frontend/src/components/StartPage.js
@@ -1,21 +1,18 @@
-import React, { useState} from "react";
-import {
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { Button, NumericInput, InputGroup, Slider } from "@blueprintjs/core";
 
-  Redirect,
-} from "react-router-dom";
-import { Button, NumericInput, InputGroup } from "@blueprintjs/core";
-
-console.log("StartPage component opened")
+console.log("StartPage component opened");
 
 function StartPage(BrowserRouter, world_state) {
-  const [input1, setInput1] = useState("5");
-  const [input2, setInput2] = useState("1");
-  const [input3, setInput3] = useState("0");
+  const [input1, setInput1] = useState(5);
+  const [input2, setInput2] = useState(0);
+  const [input3, setInput3] = useState(1);
   const [input4, setInput4] = useState("11246tq6735");
   const [data, setData] = useState(null);
   const [shouldISwitch, setShouldISwitch] = useState(false);
 
-  console.log("StartPage Function Open")
+  console.log("StartPage Function Open");
 
   function sendGameUrl() {
     /*local hostname of comp
@@ -32,9 +29,9 @@ function StartPage(BrowserRouter, world_state) {
       input3 +
       "&hand=" +
       input4;
-      console.log(uri);
+    console.log(uri);
     fetch(uri)
-      .then(function (response) {        
+      .then(function (response) {
         return response.json();
       })
       .then((data) => {
@@ -43,10 +40,6 @@ function StartPage(BrowserRouter, world_state) {
 
         if (resp === "Rej") {
           console.log("BAD INPUT");
-          setInput1("");
-          setInput2("");
-          setInput3("");
-          setInput4("");
         }
         if (resp === "Ack") {
           setData(data);
@@ -72,59 +65,73 @@ function StartPage(BrowserRouter, world_state) {
       <div className="App">
         <h1>Game Initialization</h1>
         <form onSubmit={handleSubmit}>
-          <h2><label htmlFor="new-todo-input" className="label__lg">
-            Please specify how many players are in the round
-          </label></h2>
+          <h2>
+            <label htmlFor="new-todo-input" className="label__lg">
+              Please specify how many players are in the round
+            </label>
+          </h2>
           <div></div>
-          <input
-            type="text"
-            id="new-todo-input"
-            onChange={(e) => setInput1(e.target.value)}
-            className="input input__lg"
-            value = {input1}
-          />
-          <div className="number-players"><NumericInput
-                    defaultValue={3} min={3} max={9} 
-                /></div>
-          
+          <div className="slider">
+            <Slider
+              value={input1}
+              min={3}
+              max={10}
+              labelStepSize={1}
+              stepSize={1}
+              onChange={(e) => {
+                setInput1(e);
+              }}
+            />
+          </div>
+
           <div></div>
-          <h3><label htmlFor="new-todo-input" className="label__lg">
-            Please specify your seating index at the table clockwise from the
-            0th player (the player left of dealer):
-          </label></h3>
+          <h3>
+            <label htmlFor="new-todo-input" className="label__lg">
+              Please specify your seating index at the table clockwise from the
+              0th player (the player left of dealer):
+            </label>
+          </h3>
           <div></div>
-          <input
-            type="text"
-            id="new-todo-input"
-            onChange={(e) => setInput2(e.target.value)}
-            className="input input__lg"
-            value={input2}
-          />
-          <div className="number-players"><NumericInput
-                    defaultValue={0} min={0} max={8} 
-                /></div>
+          <div className="slider">
+            <Slider
+              value={input2}
+              min={0}
+              max={input1-1}
+              labelStepSize={1}
+              stepSize={1}
+              onChange={(e) => {
+                console.log(e);
+                setInput2(e);
+              }}
+            />
+          </div>
+          <h4>
+            <label htmlFor="new-todo-input" className="label__lg">
+              Please specify the seating index of the player with the Ace of
+              spades (with respect to the dealer):
+            </label>
+          </h4>
           <div></div>
+          <div className="slider">
+            <Slider
+              value={input3}
+              min={0}
+              max={input1-1}
+              labelStepSize={1}
+              stepSize={1}
+              onChange={(e) => {
+                console.log(e);
+                setInput3(e);
+              }}
+            />
+          </div>
           <div></div>
-          <h4><label htmlFor="new-todo-input" className="label__lg">
-            Please specify the seating index of the player with the Ace of
-            spades (with respect to the dealer):
-          </label></h4>
-          <div></div>
-          <input
-            type="text"
-            id="new-todo-input"
-            onChange={(e) => setInput3(e.target.value)}
-            className="input input__lg"
-            value={input3}
-          />
-          <div className="number-players"><NumericInput
-                    defaultValue={0} min={0} max={8} 
-                /></div>
-          <div></div>
-          <h5><label htmlFor="new-todo-input" className="label__lg">
-            Please input a sequence of the cards in your hand e.g. 123
-            represents Cards 1, 2, and 3
-          </label></h5>
+          <h5>
+            <label htmlFor="new-todo-input" className="label__lg">
+              Please input a sequence of the cards in your hand e.g. 123
+              represents Cards 1, 2, and 3
+            </label>
+          </h5>
           <div></div>
           <input
             type="text"
@@ -134,20 +141,21 @@ function StartPage(BrowserRouter, world_state) {
             value={input4}
           />
           <div>
-	    <div class="submit-button">
-            <button type="submit" className="btn btn__primary btn__lg" >
-              Submit
-            </button>
-	    <Button intent="success" text="Fancy Submit" onClick={() => console.log('got clicked!')} />
-	    </div>
+            <div className="submit-button">
+              <Button
+                intent="success"
+                type="submit"
+                text="Submit"
+                onClick={() => console.log("got clicked!")}
+              />
+            </div>
           </div>
-
         </form>
       </div>
     );
   }
 }
 
-console.log("StartPage Component Closed")
+console.log("StartPage Component Closed");
 
 export default StartPage;
